2b3ff90cb2b0bc09d30ad8a8b74622676c8372a6
Claudio Rojas Rodriguez <claudio@MacBook-Air-de-Claudio.local>
2017-07-25 03:09:19 -0400

    add init project

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'rutfunctions',
+  packages = ['rutfunctions'], # this must be the same as the name above
+  version = '0.1',
+  description = 'helpers to chilean rut',
+  author = 'Claudio Rojas',
+  author_email = 'claudio.dcv@gmail.com',
+  url = 'https://github.com/claudioDcv/RUTFunctionsPython', # use the URL to the github repo
+  download_url = 'https://github.com/{user_name}/{repo}/tarball/0.1',
+  keywords = ['python-functions', 'es6', 'chilean-rut-utils', 'functional-programming'],
+  classifiers = [],
+)
